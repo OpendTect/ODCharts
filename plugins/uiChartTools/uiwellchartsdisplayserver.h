e7f8678ae1520c00020d4022ddc5fa1e7a110a38
Nanne Hemstra <nanne.hemstra@dgbes.com>
2022-08-23 14:30:09 -0500

    Trying to compile code

--- plugins/uiChartTools/uiwellchartsdisplayserver.h
+++ plugins/uiChartTools/uiwellchartsdisplayserver.h
@@ -29,7 +29,7 @@ public:
 };
 
 
-mExpClass(uiWellChartsTools) uiWellChartsLogToolWinGrp
+mExpClass(uiChartTools) uiWellChartsLogToolWinGrp
 					: public uiWellLogToolWinGrp
 {
 mODTextTranslationClass(uiWellChartsDisplayServer)
